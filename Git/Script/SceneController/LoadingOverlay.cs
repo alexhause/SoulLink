47be704d911dee3ae403678812ed3ae6ee8120fc
alexhause <alexhause1@mail.ru>
2025-08-30 23:37:24 +0300

    Добавил базовый скрипт экрана перехода

---
+++ Git/Script/SceneController/LoadingOverlay.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+
+public class LoadingOverlay : MonoBehaviour
+{
+    public IEnumerator FadeIn()
+    {
+        yield return null;
+    }
+
+    public IEnumerator FadeOut()
+    {
+        yield return null;
+    }
+}
+
+
